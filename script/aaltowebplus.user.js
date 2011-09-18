7f388eee66448a9ea039db94635bd7d28897158c
Antti Hirvonen <antti.hirvonen@gmail.com>
2011-09-18 20:19:26 +0300

    Initial version of the script.
    This version includes:
        - automatic logins for Noppa/Oodi
        - search bar on the right navigation bar of Noppa
        - direct redirect to course home page in Noppa if
          there is only one search result

---
+++ script/aaltowebplus.user.js
@@ -0,0 +1,133 @@
+// ==UserScript==
+// @name                A?alto Web+
+// @namespace	        tag:antti.hirvonen@gmail.com,2011-09-16:aaltowebplus
+// @description	        Makes Aalto IT services more usable.
+// @include		https://noppa.aalto.fi/*
+// @include     https://idp.aalto.fi/idp/Authn/Kerberos
+// @include     https://weblogin.tkk.fi/app/login
+// @include     https://cardan.aalto.fi/iam/*
+// @exclude     https://noppa.aalto.fi/Shibboleth.sso/
+// @exclude     https://noppa.aalto.fi/noppa/assets/static/dojo-0.4.3/iframe_history.html
+// ==/UserScript==
+
+// A?alto Web+
+// by Antti Hirvonen (antti.hirvonen@gmail.com)
+//
+// This script makes Aalto's IT-services more usable by providing automated logins
+// for Noppa and Oodi. It also fixes some shortcomings in Noppa. See the web page
+// for more information.
+//
+// Please submit your feature requests/bug reports/etc. via Github or e-mail!
+
+function doNoppaSearch() {
+    var inputValue = document.getElementById("fastsearch-input").value;
+    inputValue = inputValue.replace(/ /g, "_");
+    inputValue = escape(inputValue);
+    window.location.href = "https://noppa.aalto.fi/noppa/haku/" + inputValue;
+    return false;
+}
+function makeNoppaMoreUsable() {
+    // Add course search bar to right side navi
+    var searchContainer = document.createElement('div');
+    searchContainer.innerHTML = '<div class="title">A?alto Web+</div>\
+                                <div>\
+                                    <input id="fastsearch-input" type="text" class="textInput" />\
+                                    <input id="fastsearch-submit" type="submit" class="inputButton" value="Hae" />\n\
+                                </div>'
+    searchContainer.className = "box";
+    
+    var cont = document.getElementById("additionalNaviContainer");
+    if(cont.firstChild) cont.insertBefore(searchContainer, cont.firstChild);
+    else cont.appendChild(searchContainer);
+    
+    document.getElementById("fastsearch-submit").addEventListener("click", doNoppaSearch, true)
+    
+    // Search results: if only one result, directly redirect
+    if(/^https:\/\/noppa\.aalto\.fi\/noppa\/haku/.test(location.href)) {
+        GM_log("onlyCourse.lastChild.firstChild.href");
+        if(document.getElementById("crsTableView") && !document.getElementById("informal_2")) {
+            var onlyCourse = document.getElementById("informal_1");
+            // redirect to course
+            window.location.href = onlyCourse.getElementsByTagName('a')[0].href;
+        }
+    }
+}
+
+function autoLoginForm(userfieldName, helpTextContainer)
+{
+    // Sets autocomplete to value "on" for any element
+    var allowAutoComplete = function(element) {
+        var iAttrCount = element.attributes.length;
+        for (var i = 0; i < iAttrCount; i++) {
+            var oAttr = element.attributes[i];
+            if (oAttr.name == 'autocomplete') {
+                oAttr.value = 'on';
+                break;
+            }
+        }
+    }
+    
+    // Call allow autocomplete on form and it's elements
+    var form = document.getElementsByTagName('form')[0];
+    allowAutoComplete(form);
+    for (var j = 0; j < form.elements.length; j++)
+    {
+        allowAutoComplete(form.elements[j]);
+    }
+    
+    // Submits login form after browser has filled in password
+    var submitLogin = function() {
+        var username = document.getElementsByName(userfieldName)[0].value;
+        GM_log(username);
+        // Submit only, if username has been given
+        if(username)
+            document.forms[0].submit();
+    };
+    
+    window.addEventListener("load", function() { 
+        // Use a setTimeout so browser's password manager has a chance to fill in the password
+        setTimeout(submitLogin, 0); 
+      }, false);
+      
+    // Show help
+    var helptext = document.createElement('h3');
+    helptext.innerHTML = 'A?alto Web+ suggests, that you save your login credentials\
+                          into your browser. That way you your browser will autocomplete these\
+                          values for you, making the login process completely automatic.<br />\
+                          <span style="color:red;">DO NOT SAVE THEM ON A PUBLIC COMPUTER.</span>';
+    helpTextContainer.appendChild(helptext);
+}
+
+// NOPPA SECTION
+if(/^https:\/\/noppa\.aalto\.fi/.test(location.href)) {
+    GM_log("NOPPA");
+    // If no element with loginName id is found, we are not logged in - redirect
+    loginNameElement = document.getElementById("loginName")
+    if(loginNameElement)
+    {
+        // We are in Noppa!
+        GM_log('Hello world!');
+        makeNoppaMoreUsable();
+    }
+    else
+        window.location.href = "https://noppa.aalto.fi:443/Shibboleth.sso/AALTOLogin?target=https://noppa.aalto.fi/noppa/shibboleth_login"
+}
+
+// OODI(?) LOGIN SELECTOR
+if(/^https:\/\/cardan\.aalto\.fi\/iam\/WAYF/.test(location.href)) {
+    GM_log("LOGIN SELECTOR");
+    var select = document.getElementsByName("user_idp")[0];
+    select.selectedIndex = 4;
+    document.forms[0].submit();
+}
+
+// AALTO LOGIN SECTION
+if(/^https:\/\/idp.aalto.fi\/idp\/Authn\/Kerberos/.test(location.href)) {
+    GM_log("LOGIN");
+    autoLoginForm("j_username", document.getElementById("infotext"));
+}
+
+// OLD TKK WEBLOGIN
+if(/^https:\/\/weblogin.tkk.fi\/app\/login/.test(location.href)) {
+    autoLoginForm("user", document.getElementById("content").getElementsByTagName("div")[0]);
+}
